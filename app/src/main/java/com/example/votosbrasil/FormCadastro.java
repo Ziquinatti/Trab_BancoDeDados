2e3b3de8137eeac17fd9ecc248f2794e225a330e
Lucas Ziquinatti Piccini Nunes <72109329+Ziquinatti@users.noreply.github.com>
2023-01-06 15:36:48 -0300

    Começo da lógica de cadastro de usuários.

--- app/src/main/java/com/example/votosbrasil/FormCadastro.java
+++ app/src/main/java/com/example/votosbrasil/FormCadastro.java
@@ -2,9 +2,20 @@ package com.example.votosbrasil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.EditText;
+import android.widget.Button;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+
+import com.blankj.utilcode.util.NetworkUtils;
+import com.google.android.material.snackbar.Snackbar;
 
 public class FormCadastro extends AppCompatActivity {
+
+    private EditText edit_nome, edit_email, edit_senha;
+    private Button bt_cadastrar;
+    String[] mensagens = {"Peencha todos os campos", "Não há conexão ativa!", "Cadastro realizado com sucesso"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +23,50 @@ public class FormCadastro extends AppCompatActivity {
         setContentView(R.layout.activity_form_cadastro);
 
         getSupportActionBar().hide();
+        IniciarComponentes();
+
+        bt_cadastrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String nome = edit_nome.getText().toString();
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+
+                if(nome.isEmpty() || email.isEmpty() || senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(v, mensagens[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                } else {
+                    CadastrarUsuario(v);
+                }
+            }
+        });
+    }
+
+    private void CadastrarUsuario(View v){
+        if(NetworkUtils.isConnected()){
+
+        } else {
+            Snackbar snackbar = Snackbar.make(v, mensagens[1], Snackbar.LENGTH_SHORT);
+            snackbar.setBackgroundTint(Color.WHITE);
+            snackbar.setTextColor(Color.BLACK);
+            snackbar.show();
+        }
+    }
+
+    private boolean validarCampos(){
+        edit_nome.setError("");
+        edit_email.setError("");
+        edit_senha.setError("");
+
+        return true;
+    }
+
+    private void IniciarComponentes(){
+        edit_nome = findViewById(R.id.edit_nome);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        bt_cadastrar = findViewById(R.id.bt_cadastrar);
     }
 }
---
+++ app/src/main/java/com/example/votosbrasil/Usuario.java
@@ -0,0 +1,8 @@
+package com.example.votosbrasil;
+
+public class Usuario {
+    public String nome;
+    public String email;
+    public String senha;
+    public String confirmarSenha;
+}
